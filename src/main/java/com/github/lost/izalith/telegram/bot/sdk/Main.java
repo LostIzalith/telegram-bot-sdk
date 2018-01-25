57c38c3f25b9873da2d8be02d112462a998f07cf
solairerove <hax2033@gmail.com>
2018-01-25 15:22:32 +0300

    feature: add gitignore and maven framework support

---
+++ src/main/java/com/github/lost/izalith/telegram/bot/sdk/Main.java
@@ -0,0 +1,7 @@
+package com.github.lost.izalith.telegram.bot.sdk;
+
+public class Main {
+    public static void main(final String... args) {
+        System.out.println("# telegram-bot-sdk");
+    }
+}
